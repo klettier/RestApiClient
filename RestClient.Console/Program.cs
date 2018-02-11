73b50652e475c863790ca2ac3f6fd9125f83adea
klettier <lettier.kevin@gmail.com>
2018-02-11 21:58:24 +0100

    Work on proposition to simplify things, Add  new project and test console

---
+++ RestClient.Console/Program.cs
@@ -0,0 +1,231 @@
+﻿using SymphonyOSS.RestApiClient.Generated.OpenApi.AgentApi;
+using SymphonyOSS.RestClient.Generated.OpenApi.AuthenticatorApi;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RestClient.Console
+{
+    class Program
+    {
+        const string agentUrl = "https://foundation-dev.symphony.com/agent";
+        const string keyauthUrl = "https://foundation-dev-api.symphony.com/keyauth";
+        const string sessionauthUrl = "https://foundation-dev-api.symphony.com/sessionauth";
+
+        //Create a file containing one line like: C:\toto.p12;myPassword
+        static readonly string[] pass = File.ReadAllLines(@"C:\Symphony\Pass.txt").First().Split(';').ToArray();
+
+        static readonly X509Certificate2 cert = new X509Certificate2(pass[0], pass[1]);
+        static Lazy<HttpClient> certClient = new Lazy<HttpClient>(() => CreateHttpClient(() => CreateCertHandler(cert)));
+        static Lazy<HttpClient> secuClient = new Lazy<HttpClient>(() => CreateHttpClient(() => CreateSecuHandler(cert)));
+        static Lazy<HttpClient> secuClientRetry = new Lazy<HttpClient>(() => CreateHttpClient(() => CreateSecuRetryHandler(cert)));
+
+        static HttpClient CreateHttpClient(Func<HttpMessageHandler> handlerFactory)
+        {
+            var handler = handlerFactory();
+
+            return new HttpClient(handler);
+        }
+
+        static Lazy<IStreamClient> streamClient = new Lazy<IStreamClient>(() =>
+        {
+            return new StreamClient(agentUrl, secuClientRetry.Value);
+        });
+
+        static IAuthenticateClient sessionAuthApi = new AuthenticateClient(sessionauthUrl, certClient.Value);
+        static IAuthenticateClient keyAuthApi = new AuthenticateClient(keyauthUrl, certClient.Value);
+
+        static string sessionToken;
+        static string keyManagerToken;
+        static object synch = new object();
+
+        static SemaphoreSlim mySemaphoreSlim = new SemaphoreSlim(1, 1);
+        static DateTimeOffset lastBuild = default;
+
+        static bool CanGoIn()
+        {
+            return
+                lastBuild == default ||
+                (DateTimeOffset.Now - lastBuild) > TimeSpan.FromMinutes(1);
+        }
+
+        static async Task BuildTokens()
+        {
+            if (!CanGoIn())
+                return;
+
+            await mySemaphoreSlim.WaitAsync();
+
+            try
+            {
+                if (!CanGoIn())
+                    return;
+
+                sessionToken = (await sessionAuthApi.V1Async()).Token1;
+                keyManagerToken = (await keyAuthApi.V1Async()).Token1;
+
+                System.Console.WriteLine($"Generate tokens{Environment.NewLine}{nameof(sessionToken)}: {sessionToken}{Environment.NewLine}{nameof(keyManagerToken)}: {keyManagerToken}");
+
+                lastBuild = DateTimeOffset.Now;
+            }
+            finally
+            {
+                mySemaphoreSlim.Release();
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            await BuildTokens();
+
+            var datafeedApi = new DatafeedClient(agentUrl, certClient.Value);
+            var datafeed = await datafeedApi.V4CreateAsync(sessionToken, keyManagerToken);
+
+            while (true)
+            {
+                try
+                {
+                    System.Console.WriteLine("Start reading !");
+                    var events = await datafeedApi.V4ReadAsync(datafeed.Id, sessionToken, keyManagerToken);
+
+                    await Play(events);
+                }
+                catch (SymphonyOSS.RestApiClient.Generated.OpenApi.AgentApi.SwaggerException e) when (e.StatusCode == "204")
+                {
+                    System.Console.WriteLine("204 detected");
+                }
+            }
+        }
+
+        static async Task Play(IEnumerable<V4Event> events)
+        {
+            var tasks =
+                events.Select(e => Task.Factory.StartNew(async () => await Route(e)))
+                      .ToArray();
+
+            await Task.WhenAll(tasks);
+        }
+
+        static async Task Route(V4Event @event)
+        {
+            if (@event == null ||
+               !@event.Type.HasValue)
+                return;
+
+            switch (@event.Type.Value)
+            {
+                case V4EventType.MESSAGESENT:
+                    await Talk(@event.Payload.MessageSent);
+                    break;
+            }
+        }
+
+        static async Task Talk(V4MessageSent message)
+        {
+            System.Console.WriteLine($"Received {message.Message.Message}");
+
+            if (message.Message.User.UserId != 346621040656630)
+            {
+                using (var sR = new StringReader(message.Message.Message))
+                {
+                    var doc = XDocument.Load(sR);
+                    var messageContent = doc.Root.Value;
+                    var messageSubmit = $@"<messageML>You say ""{messageContent}"" to me the bot</messageML>";
+                    await streamClient.Value.V4MessageCreateAsync(message.Message.Stream.StreamId, messageSubmit);
+                }
+            }
+        }
+
+        internal static HttpMessageHandler CreateCertHandler(X509Certificate2 cert)
+        {
+            HttpClientHandler certHandler = new HttpClientHandler();
+            certHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            certHandler.ClientCertificates.Add(cert);
+
+            return certHandler;
+        }
+
+        internal static HttpMessageHandler CreateSecuHandler(X509Certificate2 cert)
+        {
+            HttpMessageHandler certHandler = CreateCertHandler(cert);
+
+            var headersProvider = new(string Key, Func<string> provider)[]
+            {
+                ("keyManagerToken", () => keyManagerToken),
+                ("sessionToken", () => sessionToken)
+            };
+
+            return new SecurityHandler(headersProvider, certHandler);
+        }
+
+        internal static HttpMessageHandler CreateSecuRetryHandler(X509Certificate2 cert)
+        {
+            var securityHandler = CreateSecuHandler(cert);
+
+            Func<HttpResponseMessage, Func<Task<HttpResponseMessage>>, Task<HttpResponseMessage>> fallBack =
+                async (response, f) =>
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        await BuildTokens();
+                        return await f();
+                    }
+
+                    return response;
+                };
+
+            return new RetryHandler(fallBack, securityHandler);
+        }
+
+        public class SecurityHandler : DelegatingHandler
+        {
+            private readonly IEnumerable<(string Key, Func<string> provider)> headersProvider;
+
+            public SecurityHandler(IEnumerable<(string Key, Func<string> provider)> headersProvider, HttpMessageHandler innerHandler) : base(innerHandler)
+            {
+                this.headersProvider = headersProvider;
+            }
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                foreach ((string key, Func<string> provider) in headersProvider)
+                {
+                    request.Headers.Remove(key);
+
+                    var val = provider();
+                    request.Headers.Add(key, val);
+
+                    System.Console.WriteLine($"Set header{Environment.NewLine}{key}: {val}");
+                }
+
+                return await base.SendAsync(request, cancellationToken);
+            }
+        }
+
+        public class RetryHandler : DelegatingHandler
+        {
+            private readonly Func<HttpResponseMessage, Func<Task<HttpResponseMessage>>, Task<HttpResponseMessage>> fallBack;
+
+            public RetryHandler(Func<HttpResponseMessage, Func<Task<HttpResponseMessage>>, Task<HttpResponseMessage>> fallBack, HttpMessageHandler innerHandler) : base(innerHandler)
+            {
+                this.fallBack = fallBack;
+            }
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                Func<Task<HttpResponseMessage>> f = async () => await base.SendAsync(request, cancellationToken);
+
+                var t = await f();
+
+                return await fallBack(t, f);
+            }
+        }
+    }
+}
---
+++ src/SymphonyOSS.RestClient/Generated/OpenApi/generate.bat
@@ -0,0 +1,65 @@
+@rem  Licensed to the Symphony Software Foundation (SSF) under one
+@rem  or more contributor license agreements.  See the NOTICE file
+@rem  distributed with this work for additional information
+@rem  regarding copyright ownership.  The SSF licenses this file
+@rem  to you under the Apache License, Version 2.0 (the
+@rem  "License"); you may not use this file except in compliance
+@rem  with the License.  You may obtain a copy of the License at
+@rem  
+@rem  http://www.apache.org/licenses/LICENSE-2.0
+@rem  
+@rem  Unless required by applicable law or agreed to in writing,
+@rem  software distributed under the License is distributed on an
+@rem  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@rem  KIND, either express or implied.  See the License for the
+@rem  specific language governing permissions and limitations
+@rem  under the License.
+
+rmdir /s /q temp
+
+mkdir temp
+
+@rem Our code generator, nswag, requires the JSON representation of the Swagger definition
+@rem so we are coverting the YAML to Json using:
+@rem https://github.com/LucyBot-Inc/api-spec-converter
+@rem this can be installed by running:
+@rem         npm install -g api-spec-converter
+call api-spec-converter Yaml\1.46\agentAPI.yaml --from=swagger_2 --to=swagger_2 --syntax=json >temp\agentAPI.json
+call api-spec-converter Yaml\1.46\authenticatorAPI.yaml --from=swagger_2 --to=swagger_2 --syntax=json >temp\authenticatorAPI.json
+call api-spec-converter Yaml\1.46\podAPI.yaml --from=swagger_2 --to=swagger_2 --syntax=json >temp\podAPI.json
+
+@rem How nswag interprets the paths does not work well with how Symphony has defined their paths, so 
+@rem we will manually create operationIds that will force nswag to create the class and method names
+@rem that we want
+
+msbuild ..\..\..\..\tools\tools.sln
+dotnet ..\..\..\..\tools\SwaggerOperationIdGenerator\SwaggerOperationIdGenerator\bin\Debug\netcoreapp1.1\SwaggerOperationIdGenerator.dll temp\agentAPI.json >temp\agentAPI.operations.json
+dotnet ..\..\..\..\tools\SwaggerOperationIdGenerator\SwaggerOperationIdGenerator\bin\Debug\netcoreapp1.1\SwaggerOperationIdGenerator.dll temp\authenticatorAPI.json >temp\authenticatorAPI.operations.json
+dotnet ..\..\..\..\tools\SwaggerOperationIdGenerator\SwaggerOperationIdGenerator\bin\Debug\netcoreapp1.1\SwaggerOperationIdGenerator.dll temp\podAPI.json >temp\podAPI.operations.json
+
+
+
+rmdir /s /q AgentApi
+rmdir /s /q AuthenticatorApi
+rmdir /s /q PodApi
+
+call nswag run Config\agentAPI.nswag
+call nswag run Config\authenticatorAPI.nswag
+call nswag run Config\podAPI.nswag
+
+@rem When a request has an empty post body, nswag sets the content-type to text/plain, but Symphony
+@rem still enforces that it be application/json. Here, we simply add the mimetype to the StringContent
+powershell -Command "(gc AgentApi/AgentApi.cs) -replace 'System.Net.Http.StringContent\(string.Empty\);', 'System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, \"application/json\");' | Out-File AgentApi/AgentApi.cs"
+powershell -Command "(gc PodApi/PodApi.cs) -replace 'System.Net.Http.StringContent\(string.Empty\);', 'System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, \"application/json\");' | Out-File PodApi/PodApi.cs"
+powershell -Command "(gc AuthenticatorApi/AuthenticatorApi.cs) -replace 'System.Net.Http.StringContent\(string.Empty\);', 'System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, \"application/json\");' | Out-File AuthenticatorApi/AuthenticatorApi.cs"
+
+
+rmdir /s /q Temp
+
+call prepend_license.bat
+
+@rem apply patch to support content-type header for attachments
+@rem the delay here is to allow something from above to release
+@rem the write lock on AgentApi.cs
+timeout 5
+git apply patches\attachment.patch
